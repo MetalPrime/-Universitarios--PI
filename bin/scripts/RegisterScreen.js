e02107a45b52fb6991d6c4894e5b1dafcc03a7fc
MetalPrime <53907237+MetalPrime@users.noreply.github.com>
2019-11-15 23:17:47 -0500

    Pantalla Tres- V1

--- bin/scripts/RegisterScreen.js
+++ bin/scripts/RegisterScreen.js
@@ -1,6 +1,7 @@
 
 let check = document.querySelector(".circle-botton");
+let newImage = document.getElementById("./../../data/img/circle2.png");
 
 check.addEventListener("click",function(){
-    document.location.replace("./../../data/img/circle2.png")
+   
 })
