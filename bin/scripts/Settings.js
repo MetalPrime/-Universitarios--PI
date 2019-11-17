b9f5c2404ba3537e34cd2bac1e5c404568950402
MetalPrime <53907237+MetalPrime@users.noreply.github.com>
2019-11-17 06:08:10 -0500

    Creacion pantalla settings

---
+++ bin/scripts/Settings.js
@@ -0,0 +1,16 @@
+//Pass to Suggestions
+
+let btn = document.querySelector(".botton-main");
+
+btn.addEventListener("click",function(){
+    document.location.href="Suggestions.html"
+})
+
+//return to Suggestions
+
+let btn = document.querySelector(".back");
+
+btn.addEventListener("click",function(){
+  document.location.href="./../views/Suggestions.html";
+ 
+})
