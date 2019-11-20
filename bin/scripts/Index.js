4a98f9a6a481609e81c562cd64756d42d32e5077
Your Name <sebascufu1705@hotmail.com>
2019-11-20 08:51:52 -0500

    realize el guardado de la pagina registrar en fireBase

--- bin/scripts/Index.js
+++ bin/scripts/Index.js
@@ -3,23 +3,7 @@ let btn = document.querySelector(".botton-main")
 let body = document.querySelector(".body");
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDcw9TxeFA8dofUkpAQE-i4buNWHV8qHpA",
-    authDomain: "universitarios-86d73.firebaseapp.com",
-    databaseURL: "https://universitarios-86d73.firebaseio.com",
-    projectId: "universitarios-86d73",
-    storageBucket: "universitarios-86d73.appspot.com",
-    messagingSenderId: "557583268134",
-    appId: "1:557583268134:web:0ddcd718c5a356a0bee9e7",
-    measurementId: "G-6HCQC6LETL"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-
-// Get a reference to the database service
-var database = firebase.database();
 
 btn.addEventListener("click", function () {
 body.classList.add("animated")
--- bin/scripts/RegisterScreen.js
+++ bin/scripts/RegisterScreen.js
@@ -1,3 +1,27 @@
+var firebaseConfig = {
+  
+    apiKey: "AIzaSyAR-JARidY_1aTaYZKdQnJhAAv4YPupL2Q",
+    authDomain: "universitariospi.firebaseapp.com",
+    databaseURL: "https://universitariospi.firebaseio.com",
+    projectId: "universitariospi",
+    storageBucket: "universitariospi.appspot.com",
+    messagingSenderId: "503987964464",
+    appId: "1:503987964464:web:7058acab764c913c2bbbf5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+  
+  
+  // Get a reference to the database service
+  var database = firebase.database();
+  /////////////////////////////////////
+  ///////////////////////////////////////
+  let nombre = document.querySelector(".nombre")
+  let correo = document.querySelector(".correo")
+  let contraseña = document.querySelector(".contraseña")
+  let confirmarContraseña = document.querySelector(".confirmarContraseña")
 // cHECK
 let check = document.querySelector(".circle-botton");
 
@@ -10,5 +34,19 @@ check.addEventListener("click",function(){
 let btn = document.querySelector(".botton-main");
 
 btn.addEventListener("click",function(){
-    document.location.href="./../views/Suggestions.html";
+    let nombreInput = nombre.value;
+    let correoInput = correo.value;
+    let contraseñaInput = contraseña.value;
+    let confirmarConInput = confirmarContraseña.value;
+
+ 
+ if(contraseñaInput==confirmarConInput){firebase.database().ref('usuarios/').push({
+    nombre: nombreInput, 
+    correo: correoInput,
+    contraseña: contraseñaInput
+    
+});} else{
+    alert("no coninciden contraeñas");
+} 
+   // document.location.href="./../views/Suggestions.html";
 })
