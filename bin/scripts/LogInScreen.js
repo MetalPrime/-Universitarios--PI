153b001d0bd91f7aa689d3e1f66aac06646b7339
Your Name <sebascufu1705@hotmail.com>
2019-11-20 10:23:55 -0500

    realice el guardado de usuarios y la accesibilidad al login de la aplicacion

--- bin/scripts/LogInScreen.js
+++ bin/scripts/LogInScreen.js
@@ -1,5 +1,75 @@
-let btn = document.querySelector(".botton-main");
+var firebaseConfig = {
 
-btn.addEventListener("click",function(){
-    document.location.href="./../views/Suggestions.html";
+    apiKey: "AIzaSyAR-JARidY_1aTaYZKdQnJhAAv4YPupL2Q",
+    authDomain: "universitariospi.firebaseapp.com",
+    databaseURL: "https://universitariospi.firebaseio.com",
+    projectId: "universitariospi",
+    storageBucket: "universitariospi.appspot.com",
+    messagingSenderId: "503987964464",
+    appId: "1:503987964464:web:7058acab764c913c2bbbf5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
+let btn = document.querySelector(".botton-main");
+let correoInput = document.querySelector(".correoInput")
+let passwordInput = document.querySelector(".passwordInput")
+
+
+
+
+btn.addEventListener("click", function () {
+    let correo = correoInput.value
+    correo = correo.toLowerCase()
+
+    let contraseña = passwordInput.value
+
+
+    let usuarios = firebase.database().ref("usuarios/");
+    usuarios.on('value', function (snapshot) {
+
+        //snapshotVal() recoge todos los elementos del firebase
+        let completeData = snapshot.val();
+
+        //Object.Keys() transforma todos los elemntos en un arreglo
+        let arregloDeDatos = Object.keys(completeData)
+
+        for (let i = 0; i < arregloDeDatos.length; i++) {
+
+            let id = arregloDeDatos[i]
+            let nombre = completeData[id].nombre;
+            let correoDB = completeData[id].correo;
+            let contraseñaDB = completeData[id].contraseña;
+
+
+            if (correo == correoDB) {
+
+                if (contraseña == contraseñaDB) {
+                    document.location.href = "./../views/Suggestions.html";
+                }else{
+                    alert("Contraseña es incorrecta")
+                }
+
+            }
+        }
+
+    }, function (err) {
+        console.log(err)
+    });
+
+
+
+
 })
+
+
+
+
+
--- bin/scripts/RegisterScreen.js
+++ bin/scripts/RegisterScreen.js
@@ -1,5 +1,5 @@
 var firebaseConfig = {
-  
+
     apiKey: "AIzaSyAR-JARidY_1aTaYZKdQnJhAAv4YPupL2Q",
     authDomain: "universitariospi.firebaseapp.com",
     databaseURL: "https://universitariospi.firebaseio.com",
@@ -7,46 +7,71 @@ var firebaseConfig = {
     storageBucket: "universitariospi.appspot.com",
     messagingSenderId: "503987964464",
     appId: "1:503987964464:web:7058acab764c913c2bbbf5"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  //firebase.analytics();
-  
-  
-  // Get a reference to the database service
-  var database = firebase.database();
-  /////////////////////////////////////
-  ///////////////////////////////////////
-  let nombre = document.querySelector(".nombre")
-  let correo = document.querySelector(".correo")
-  let contraseña = document.querySelector(".contraseña")
-  let confirmarContraseña = document.querySelector(".confirmarContraseña")
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+
+// Get a reference to the database service
+var database = firebase.database();
+/////////////////////////////////////
+///////////////////////////////////////
+let nombre = document.querySelector(".nombre")
+let correo = document.querySelector(".correo")
+let contraseña = document.querySelector(".contraseña")
+let confirmarContraseña = document.querySelector(".confirmarContraseña")
+let aceptar = false;
 // cHECK
 let check = document.querySelector(".circle-botton");
 
-check.addEventListener("click",function(){
-   document.querySelector(".circle-botton").src = "./../../data/img/circle2.png";
+check.addEventListener("click", function () {
+    aceptar = !aceptar;
+    if (aceptar == true) {
+        document.querySelector(".circle-botton").src = "./../../data/img/circle2.png";
+    } else if (aceptar == false) {
+        document.querySelector(".circle-botton").src = "./../../data/img/circle.png";
+    }
+
 })
 
 //Pass to Suggestions
 
 let btn = document.querySelector(".botton-main");
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function () {
     let nombreInput = nombre.value;
     let correoInput = correo.value;
+    correoInput=correoInput.toLowerCase();
     let contraseñaInput = contraseña.value;
     let confirmarConInput = confirmarContraseña.value;
 
- 
- if(contraseñaInput==confirmarConInput){firebase.database().ref('usuarios/').push({
-    nombre: nombreInput, 
-    correo: correoInput,
-    contraseña: contraseñaInput
-    
-});} else{
-    alert("no coninciden contraeñas");
-} 
-   // document.location.href="./../views/Suggestions.html";
+
+    if (contraseñaInput == confirmarConInput && aceptar == true && correoInput.includes("@") && correoInput.includes(".com")
+        && nombreInput.length > 0 && confirmarConInput.length > 0) {
+        firebase.database().ref('usuarios/').push({
+            nombre: nombreInput,
+            correo: correoInput,
+            contraseña: contraseñaInput
+
+        });
+        document.location.href = "./../views/Suggestions.html";
+
+    } else if (contraseñaInput != confirmarConInput) {
+        alert("no coninciden contraeñas");
+    }
+    if (nombreInput.length == 0) {
+        alert("ingrese un nombre");
+    }
+    if (contraseñaInput.length == 0) {
+        alert("ingrese una contraseña");
+    }
+    if (aceptar == false) {
+        alert("no aceptaste los terminos y condiciones")
+    }
+    if (correoInput.includes("@") == false || correoInput.includes(".com") == false) {
+        alert("ingrese un correo electronico valido");
+    }
 })
+
