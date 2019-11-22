cbcb2863c4bff46eefbc23b2fd8ec336c1ee581b
Juan1899 <53906952+Juan1899@users.noreply.github.com>
2019-11-22 14:56:50 -0500

    Reenviar al enlace el QR

--- bin/scripts/Camara.js
+++ bin/scripts/Camara.js
@@ -1,23 +1,34 @@
-
-let camara= document.querySelector("#navigation-elements");
-let search= document.querySelector("#search-botton");
-let contact= document.querySelector("#contact-botton");
+let url = document.querySelector('#cam-qr-result');
+let camara = document.querySelector("#navigation-elements");
+let search = document.querySelector("#search-botton");
+let contact = document.querySelector("#contact-botton");
 
 
 // boton para ir a camara
 
-camara.addEventListener("click",function(){
-    document.location.href="./../views/Camara.html";
+camara.addEventListener("click", function () {
+    document.location.href = "./../views/Camara.html";
 })
 
 // boton para ir a search
 
-search.addEventListener("click",function(){
-    document.location.href="./../views/Suggestions.html";
+search.addEventListener("click", function () {
+    document.location.href = "./../views/Suggestions.html";
 })
 
 // boton para ir a collaborator
 
-contact.addEventListener("click",function(){
-    document.location.href="./../views/Collaborator.html";
+contact.addEventListener("click", function () {
+    document.location.href = "./../views/Collaborator.html";
 })
+
+// redirect
+
+let contador = setInterval(function () {
+    if (url.innerHTML != "") {
+
+        document.location.href = url.innerHTML
+    }
+
+
+}, 1000)
