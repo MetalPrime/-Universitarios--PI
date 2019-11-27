b5a6b90497a834b19996ee45f4221d50891e972b
estudiante <estudiante@SC302E12.local>
2019-11-26 22:44:55 -0500

    Otras Infografias

---
+++ bin/scripts/infografia3.js
@@ -0,0 +1,10 @@
+let download = document.querySelector(".descargar");
+let back = document.querySelector(".back");
+
+//
+
+back.addEventListener("click", function () {
+    document.location.href = "./../views/Suggestions.html";
+})
+
+//
